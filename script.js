5d643361aacb61237f6c9f8262f133ea08592820
AsrielG <163115932+AsrielG@users.noreply.github.com>
2025-02-12 20:59:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Website loaded");
+
+
+
+
+
+
+
+// function searchFunction() {
+//     let input = document.getElementById("searchBar").value.toLowerCase();
+//     let items = document.querySelectorAll("#itemList li");
+//     let list = document.getElementById("itemList");
+
+//     let hasResults = false;
+
+//     items.forEach(item => {
+//         let text = item.textContent.toLowerCase();
+//         if (input && text.includes(input)) {
+//             item.style.display = "block";
+//             hasResults = true;
+//         } else {
+//             item.style.display = "none";
+//         }
+//     });
+
+//     list.style.display = hasResults ? "block" : "none";
+// }
