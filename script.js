76d31168baf7937ece85a02d2bd7e4eb446b6036
AsrielG <163115932+AsrielG@users.noreply.github.com>
2025-03-11 19:38:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.querySelectorAll("nav a").forEach(link => {
   
     link.addEventListener("click", function(event) {
       
-        event.preventDefault(); 
-        const targetId = this.getAttribute("href"); 
-        const section = document.querySelector(targetId);
+        event.preventDefault();  
+        const targetId = this.getAttribute("href");  
+        const section = document.querySelector(targetId);  
         const index = [...document.querySelectorAll("section")].indexOf(section);
-        
+
         document.querySelector(".slider").style.transform = 
             `translateX(-${index * 100}vw)`;
       
