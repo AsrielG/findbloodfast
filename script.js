24cfdac8c455e48cefd03e3e60ae1d671d739473
AsrielG <163115932+AsrielG@users.noreply.github.com>
2025-03-11 20:19:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+document.querySelectorAll("nav a").forEach(link => {
+  
+    link.addEventListener("click", function(event) {
+      
+        event.preventDefault();
+        const targetId = this.getAttribute("href"); 
+        const section = document.querySelector(targetId);
+        document.querySelector(".slider").style.transform = 
+            `translateX(-${section.offsetLeft}px)`;
+      
+    });
+});
