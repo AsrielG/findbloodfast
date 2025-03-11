19dd0d063113917917de8ecfcaeb3ae8178d5738
AsrielG <163115932+AsrielG@users.noreply.github.com>
2025-03-11 19:41:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 document.querySelectorAll("nav a").forEach(link => {
-  
     link.addEventListener("click", function(event) {
-      
-        event.preventDefault();  
-        const targetId = this.getAttribute("href");  
-        const section = document.querySelector(targetId);  
-        const index = [...document.querySelectorAll("section")].indexOf(section);
-
+        event.preventDefault();
+        const targetId = this.getAttribute("href"); 
+        const section = document.querySelector(targetId);
         document.querySelector(".slider").style.transform = 
-            `translateX(-${index * 100}vw)`;
-      
+            `translateX(-${section.offsetLeft}px)`;
     });
 });
